8fb28205eec34edbe827e607e291de54ba3065b3
danjame <danjamexie@gmail.com>
2021-01-01 23:35:26 +0800

    building logic of editCreate section and lesson

--- src/views/course/section.vue
+++ src/views/course/section.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="course-section">
-    <el-card>
+    <el-card style="min-width: 800px">
       <div slot="header" class="header">
         <span>课程：{{ courseName }}</span>
-        <el-button type="primary" icon="el-icon-plus" @click="isSectionVisible = true">添加章节</el-button>
+        <el-button type="primary" icon="el-icon-plus" @click="handleEditCreateSection">添加章节</el-button>
       </div>
       <!-- 章节和课时列表 -->
       <el-tree
@@ -18,31 +18,31 @@
           <span>{{ node.label }}</span>
           <!-- 章节 -->
           <span v-if="data.sectionName" class="actions">
-            <el-button @click.stop="isSectionVisible = true">编辑</el-button>
-            <el-button type="primary" @click="isLessonVisible = true">添加课时</el-button>
+            <el-button @click.stop="handleEditCreateSection(data)">编辑</el-button>
+            <el-button type="primary" @click.stop="handleEditCreateLesson">添加课时</el-button>
             <el-button>状态</el-button>
           </span>
           <!-- 课时 -->
           <span v-else class="actions">
-            <el-button @click="isLessonVisible = true">编辑</el-button>
+            <el-button @click.stop="handleEditCreateLesson(data)">编辑</el-button>
             <el-button type="success">上传视频</el-button>
             <el-button>状态</el-button>
           </span>
         </div>
       </el-tree>
       <!-- 编辑添加阶段 -->
-      <el-dialog title="章节信息" :visible.sync="isSectionVisible" width="40%">
-        <el-form :model="section" label-width="80px">
+      <el-dialog title="章节信息" :visible.sync="isSectionVisible">
+        <el-form :model="section" :rules="sectionRules" ref="sectionForm" label-width="80px">
           <el-form-item label="课程名称">
             <el-input disabled :placeholder="courseName"></el-input>
           </el-form-item>
-          <el-form-item label="章节名称">
+          <el-form-item label="章节名称" prop="sectionName">
             <el-input v-model="section.sectionName"></el-input>
           </el-form-item>
-          <el-form-item label="章节描述">
+          <el-form-item label="章节描述" prop="description">
             <el-input v-model="section.description"></el-input>
           </el-form-item>
-          <el-form-item label="章节排序">
+          <el-form-item label="章节排序" prop="orderNum">
             <el-input v-model.number="section.orderNum">
               <template slot="append">数字控制排序，数字越大越靠后</template>
             </el-input>
@@ -54,30 +54,30 @@
         </div>
       </el-dialog>
       <!-- 编辑添加课时 -->
-      <el-dialog title="课时信息" :visible.sync="isLessonVisible" width="40%">
-        <el-form :model="lesson" label-width="80px">
+      <el-dialog title="课时信息" :visible.sync="isLessonVisible">
+        <el-form :model="lesson" :rules="lessonRules" ref="lessonForm" label-width="80px">
           <el-form-item label="课程名称">
             <el-input disabled :placeholder="courseName"></el-input>
           </el-form-item>
           <el-form-item label="章节名称">
             <el-input disabled :placeholder="section.sectionName"></el-input>
           </el-form-item>
-          <el-form-item label="课时名称">
+          <el-form-item label="课时名称" prop="theme">
             <el-input v-model="lesson.theme"></el-input>
           </el-form-item>
-          <el-form-item label="时长">
+          <el-form-item label="时长" prop="duration">
             <el-input v-model.number="lesson.duration">
               <template slot="append">分钟</template>
             </el-input>
           </el-form-item>
-          <el-form-item label="是否开放试听">
+          <el-form-item label="是否开放试听" prop="isFree">
             <el-switch
               v-model="lesson.isFree"
               active-color="#13ce66"
               inactive-color="#ff4949">
             </el-switch>
           </el-form-item>
-          <el-form-item label="排序">
+          <el-form-item label="排序" prop="orderNum">
             <el-input v-model.number="lesson.orderNum">
               <template slot="append">数字控制排序，数字越大越靠后</template>
             </el-input>
@@ -96,6 +96,7 @@
 import Vue from 'vue'
 import { getSectionLesson, saveOrUpdateSection } from '@/services/course-section'
 import { saveOrUpdateLesson } from '@/services/course-lesson'
+import { Form } from 'element-ui'
 
 export default Vue.extend({
   name: 'CourseSection',
@@ -122,6 +123,10 @@ export default Vue.extend({
         orderNum: 0,
         status: 0
       },
+      sectionRules: {
+        courseName: [{ required: true, message: '请输入课程名称', trigger: 'blur' }],
+        sectionName: [{ required: true, message: '请输入章节名称', trigger: 'blur' }]
+      },
       // 课时
       isLessonVisible: false,
       lesson: {
@@ -133,6 +138,11 @@ export default Vue.extend({
         isFree: false,
         orderNum: 0,
         status: 0
+      },
+      lessonRules: {
+        theme: [{ required: true, message: '请输入课时名称', trigger: 'blur' }],
+        duration: [{ required: true, message: '请输入时长', trigger: 'blur' }],
+        isFree: [{ required: true, trigger: 'blur' }]
       }
     }
   },
@@ -174,6 +184,32 @@ export default Vue.extend({
         this.$message.error(`排序失败：${err}`)
       }
       this.isLoading = false
+    },
+    handleEditCreateSection (data: any) {
+      this.isSectionVisible = true
+      if (data.courseId) {
+        // 编辑
+        console.log('编辑章节')
+        this.$nextTick(() => {
+          this.section = data
+        })
+      } else {
+        // 添加
+        console.log('添加章节');
+        (this.$refs.sectionForm as Form).resetFields()
+      }
+    },
+    handleEditCreateLesson (data: any) {
+      this.isLessonVisible = true
+      if (data.courseId) {
+        // 编辑
+        this.lesson = data
+      } else {
+        // 添加
+        if (this.$refs.lessonFrom) {
+          (this.$refs.lessonFrom as Form).resetFields()
+        }
+      }
     }
   }
 })
@@ -196,5 +232,9 @@ export default Vue.extend({
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+::v-deep .el-dialog{
+  min-width: 400px;
 }
 </style>
